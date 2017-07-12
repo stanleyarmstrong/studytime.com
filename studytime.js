f449188b9938cfde3b92a5d2e9d45790341aadd0
stonewallstan <stanleyarmstrong31@gmail.com>
2017-07-12 16:29:00 -0700

    I made some changes, I'll go back to working on it tomorrow

--- studytime.js
+++ studytime.js
@@ -4,3 +4,12 @@ var seconds = 60;
 function myFunction(){
   document.getElementById('myDropdown').classList.toggle("show");
 }
+window.onclick = function(event){
+  if(!event.target.matches('.dropbtn')){
+    var dropdowns = document.getElementsByClassName('dropdown-content');
+    for(var i = 0; i < dropdowns.length; i++){
+      var openDropdown = dropdowns[i];
+      
+    }
+  }
+}
