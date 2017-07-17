a06171d62d68df6ba73c622149e6a6f29d218c4b
Stanley Armstrong <stanleyarmstrong31@gmail.com>
2017-07-17 14:26:06 -0700

    Making three functions just to test it out

--- studytime.js
+++ studytime.js
@@ -1,15 +1,12 @@
 var hours = 24;
 var minutes = 60;
 var seconds = 60;
-function myFunction(){
-  document.getElementById('myDropdown').classList.toggle("show");
+function dropdownHours{
+
 }
-window.onclick = function(event){
-  if(!event.target.matches('.dropbtn')){
-    var dropdowns = document.getElementsByClassName('dropdown-content');
-    for(var i = 0; i < dropdowns.length; i++){
-      var openDropdown = dropdowns[i];
-      
-    }
-  }
+function dropdownMinutes{
+
+}
+function dropdownSeconds{
+  
 }
