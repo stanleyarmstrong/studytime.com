6f1a109671be1b9864bec707069924e99f1fcd45
Stanley Armstrong <stanleyarmstrong31@gmail.com>
2017-07-17 16:58:38 -0700

    I am making progress on the dropdown. I got the numbers to appear, just not where I want them to appear. I added a comment describing what the function does on click.

--- studytime.js
+++ studytime.js
@@ -1,12 +1,18 @@
 var hours = 24;
 var minutes = 60;
 var seconds = 60;
-function dropdownHours{
+$('#hours').click(function(){
+    for( var i = hours; i > 0; i--){
+      var a = document.createElement("A");
+      var result =document.createTextNode(i);
+      a.appendChild(result);
+      document.body.appendChild(a);
+    }
+  });
+  //allows the user to choose their selected amount of hours
+function dropdownMinutes(){
 
 }
-function dropdownMinutes{
+function dropdownSeconds( ){
 
-}
-function dropdownSeconds{
-  
 }
