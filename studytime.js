9dd4f3a6e0c665dbe58425a4b0fb7224c90b92db
Stanley Armstrong <stanleyarmstrong31@gmail.com>
2017-07-18 10:25:54 -0700

    Got it working and then something went wrong. Finding bug in my next pom

--- studytime.js
+++ studytime.js
@@ -3,12 +3,13 @@ var minutes = 0;
 var seconds = 0;
 function submitTime(){
   var hoursInput = document.getElementById('hours');
-  var minutesInput = document.getElementsByClassName('minutes');
-  if(hoursInput.checkvalidity() == false && minutesInput.checkvalidity() == false){
+  var minutesInput = document.getElementById('minutes');
+  var secondsInput = document.getElementById('seconds');
+  if(hoursInput.checkValidity() == false || minutesInput.checkValidity() == false || secondsInput.checkValidity()){
     alert('The amount of time you inserted is not valid. Try a different amount of time!');
   }
   else{
-    console.log("Proceeding to timer portion of app.Buyers beware");
+    console.log("Proceeding to timer portion of app. Buyers beware");
     return true;
   }
 }
