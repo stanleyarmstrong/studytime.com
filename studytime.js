f97b9f12084c61fb62a3018b1d162d35ea7cc277
Stanley Armstrong <stanleyarmstrong31@gmail.com>
2017-07-18 14:26:27 -0700

    Making finishing touches to the submitTime() function.

--- studytime.js
+++ studytime.js
@@ -3,21 +3,19 @@ var minutes = 0;
 var seconds = 0;
 var submitTime;
 function submitTime(){
-  var hoursInput = document.getElementById('hours');
-  var minutesInput = document.getElementById('minutes');
-  var secondsInput = document.getElementById('seconds');
-  if(hoursInput.checkValidity() == false || minutesInput.checkValidity() == false || secondsInput.checkValidity() == false){
+  var inputs = [document.getElementById('hours'), document.getElementById('minutes') , document.getElementById('seconds')];
+  if(inputs[0].checkValidity() == false || inputs[1].checkValidity() == false || inputs[2].checkValidity() == false){
     alert('The amount of time you inserted is not valid. Try a different amount of time!');
   }
   else{
-    console.log("Proceeding to timer portion of app. Buyers beware");
-    hours = hoursInput.value;
-    minutes = minutesInput.value;
-    if(secondsInput.value == 0){
-      seconds = 0;
-    }
-    else{
-      seconds = secondsInput.value;
+    console.log("Proceeding to timer portion of app. Buyer beware");
+    for(var i = 0; i < inputs.length(); i++){
+      if(inputs[i].value == 0){
+        inputs[i] == 0;
+      }
+      else{
+        inputs[i] = inputs[i].value;
+      }
     }
     console.log(hours);
     console.log(minutes);
@@ -27,7 +25,14 @@ function submitTime(){
     return true;
   }
 }
-
+function pomSession() {
+  hours = 0;
+  minutes = 25;
+  seconds = 0;
+  submitTime = true;
+  console.log('Starting a new Pomodoro session. Buyer beware.');
+  startTimer();
+}
 function startTimer(){
-  
+
 }
