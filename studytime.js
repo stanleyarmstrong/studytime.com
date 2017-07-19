085b697a34a11e32bcff9cf2c6f6d6dc45d9a34c
Stanley Armstrong <stanleyarmstrong31@gmail.com>
2017-07-19 10:26:45 -0700

    Got the timer counting down properly

--- studytime.js
+++ studytime.js
@@ -3,6 +3,7 @@ var minutes = 0;
 var seconds = 0;
 var time;
 var submitTimer;
+console.log("Welcome to occassionalstudying.com! Stay for a little while and listen to some great tunes.");
 $('#timeRemaining').hide();
 $('.odometer').hide();
 function submitTime(){
@@ -12,11 +13,15 @@ function submitTime(){
   }
   else{
     console.log("Proceeding to timer portion of app. Buyer beware");
+    var u = 1;
     for(var i = 0; i < inputs.length; i++){
       switch(i){
         case 0:
           if(inputs[i].value == 0){
             hours = 0;
+          }
+          else if(inputs[i].value > 0 && inputs[i].value <=9){
+            hours = '0' + inputs[i].value;
           }
           else{
             hours = inputs[i].value;
@@ -26,6 +31,9 @@ function submitTime(){
           if(inputs[i].value == 0){
             minutes = 0;
           }
+          else if(inputs[i].value > 0 && inputs[i].value <=9){
+            minutes = '0' + inputs[i].value;
+          }
           else{
             minutes = inputs[i].value;
           }
@@ -33,6 +41,9 @@ function submitTime(){
         default:
           if(inputs[i].value == 0){
             seconds = 0;
+          }
+          else if(inputs[i].value > 0 && inputs[i].value <=9){
+            seconds= '0' + inputs[i].value;
           }
           else{
             seconds = inputs[i].value;
@@ -48,7 +59,7 @@ function submitTime(){
 function pomSession() {
   hours = 0;
   minutes = 25;
-  seconds = 0;
+  seconds = 1;
   submitTimer = true;
   console.log('Starting a new Pomodoro session. Buyer beware.');
   console.log(hours);
@@ -62,8 +73,7 @@ function startTimer(){
   $('.submit').hide();
   $('.odometer').show();
   $('#timeRemaining').show();
-
-
+  deltaTime();
   if(hours <=0 && minutes <= 0 && seconds <= 0){
     alert("Congratulations, you have finished your study session. Enjoy the rest of your day!");
     window.location.reload();
@@ -71,6 +81,22 @@ function startTimer(){
 }
 function deltaTime(){
   setInterval(function(){
+    seconds--;
+    console.log(seconds);
+    if(seconds === 0 && minutes > 0){
+      seconds = 59;
+      minutes--;
+      console.log(minutes);
+      if(minutes === 0 && hours > 0){
+        minutes = 59;
+        hours--;
+        console.log(hours);
+      }
+      else if(minutes === 0){
+        minutes = 59;
+        console.log(minutes);
+      }
+    }
 
-  }, );
+  }, 1000);
 }
