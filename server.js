2a53c846316054683ff387338db724a092946d00
Stanley Armstrong <stanleyarmstrong31@gmail.com>
2017-08-09 09:51:49 -0700

    Did a little bt of server things. I am just trying to learn node then move on to the Spotify API

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 var http = require("http");
-
-http.createServer(function(request, response){
-  response.writeHead(200 , {"Content-Type" :  "text/plain"});
-  response.write("Hello World");
+var fs = require("fs");
+//404 response
+function send404Response(response){
+  response.writeHead(404 , {"Content-Type" : "text/plain"});
+  response.write("Error 404: File not Found");
   response.end();
-}).listen(8888);
+}
+function onRequest( request , response){
+  if( request.method == 'GET' && request.url == '/'){
+    response.writeHead(200 , {"Content-Type" : "text/html"});
+    fs.createReadStream("./study.html").pipe(response);
+
+  }
+  else{
+    send404Response(response);
+  }
+}
+http.createServer(onRequest).listen(8888);
